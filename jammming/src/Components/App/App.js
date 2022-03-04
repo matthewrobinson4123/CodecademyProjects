ce2b2a4c63981fa9e99887e30ead5f6bf8ad296f
Matthew Robinson <mattthew.robinson4123@gmail.com>
2022-03-04 16:58:56 -0500

    moving files and adding stuff step 40/99

---
+++ jammming/src/Components/App/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Playlist from './Playlist/Playlist';
+import SearchBar from './SearchBar/SearchBar';
+import SearchResults from './SearchResults/SearchResults';
+import React from'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [],
+      playlistName: 'PLAYLIST',
+      playlistTracks: []
+    }
+  }
+
+  render(){
+    return(
+        <div>
+          <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+            <SearchBar />
+            <div className="App-playlist">
+              <SearchResults searchResults={this.state.searchResults} />
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            </div>
+          </div>
+        </div>
+      );
+  }
+}
+
+export default App;
